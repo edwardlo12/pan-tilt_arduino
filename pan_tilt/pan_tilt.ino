3638a70b9ddcdd802d14c5dc154b87a1ad411bad
edwardssd <edwardssd@EDWARDSSD-PC>
2017-05-17 20:05:36 +0800

    serial enable

--- pan_tilt/pan_tilt.ino
+++ pan_tilt/pan_tilt.ino
@@ -13,9 +13,11 @@ int tilt_pin = 3;     //TILT控制腳位
 int x_max = 640;      //原始圖資寬
 int y_max = 480;      //原始圖資高
 double x_z, y_z, x_org, y_org, x_edge, y_edge;
-int trashold_x;      
-int trashold_y;      
+int trashold_x;
+int trashold_y;
 char buf[32];
+String incomingByte = ""; // for incoming serial data
+int led = 13;
 
 void setup() {
 
@@ -37,49 +39,70 @@ void setup() {
   int trashold_x = 10;      //x軸制動值
   int trashold_y = 10;      //y軸制動值
 
-  x_org = 0;  //初始化X原始位置
-  y_org = 0;  //初始化Y原始位置
+  x_org = 320;  //初始化X原始位置
+  y_org = 240;  //初始化Y原始位置
+
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
 
 }
 
 void loop() {
+  while (Serial.available() > 0) {
 
-  int x, y, posite;
+    int inChar = Serial.read();//not using this
 
-  
+    if (inChar != 'n') {
+      incomingByte += (char)inChar;
+    }
+    else {
+      int x, y;
 
-  x = Serial.read();
-  y = Serial.read();
-  /*x = posite & 0xFF00;
-  x = x >> (0xFF-1);
-  y = posite & 0x00FF;*/
-  
+      x = int(incomingByte[0] - '0') * 100 + int(incomingByte[1] - '0') * 10 + int(incomingByte[2] - '0') * 1;
+      y = int( incomingByte[4] - '0') * 100 + int(incomingByte[5] - '0') * 10 + int(incomingByte[6] - '0') * 1;
+      x = CV_X(x);
+      y = CV_Y(y);
+      if (abs(x_org + x) < x_edge || abs(y_org + y) < y_edge)
+      {
+        if (abs(x) > trashold_x || abs(y) > trashold_y)
+        {
+          pan.write(90 + atan(x_z / (double)(x_org + x))*rad2deg);
+          tilt.write(90 + atan(y_z / (double)(y_org + y))*rad2deg);
+        }
+      }
+      else
+      {
+        if (x > 0)
+          pan.write(180);
+        else
+          pan.write(0);
+
+        if (y > 0)
+          tilt.write(180);
+        else
+          tilt.write(0);
+      }
+      incomingByte = "";
+    }
+    digitalWrite(led, HIGH);
+    delay(500);
+  }
+  digitalWrite(led, LOW);
+  delay(500);
   //x=320,y=240;
-  
-  //sprintf(buf, "x=%f  y=%f\n", x, y);     //確認x、y值是否正確
+
+  //sprintf(buf, "x=%f  y=%f\n", x, y);     //確認x、y值是否正確(無法使用)
   //Serial.print(buf);
 
-  if (abs(x_org + x) < x_edge || abs(y_org + y) < y_edge)
-  {
-    if (abs(x) > trashold_x || abs(y) > trashold_y)
-    {
-      pan.write(90 + atan(x_z / (double)(x_org + x))*rad2deg);
-      tilt.write(90 + atan(y_z / (double)(y_org + y))*rad2deg);
-    }
-  }
-  else
-  {
-    if (x > 0)
-      pan.write(180);
-    else
-      pan.write(0);
-
-    if (y > 0)
-      tilt.write(180);
-    else
-      tilt.write(0);
-  }
-
-
-
 }
+
+int CV_X(int x)
+{
+  return x - x_max / 2;
+}
+
+int CV_Y(int y)
+{
+  return y - y_max / 2;
+}
+
